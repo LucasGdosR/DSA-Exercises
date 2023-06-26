e89f11f99b1d4dbe017ea1e7dfc041d9369f257f
LucasGdosR <109366863+LucasGdosR@users.noreply.github.com>
2023-06-26 12:59:04 -0300

    Further optimize UnionFind.java

--- union_find/UnionFind.java
+++ union_find/UnionFind.java
@@ -3,10 +3,12 @@ package union_find;
 public class UnionFind {
     protected int[] id;
     protected int[] size;
+    private int[] toCompress;
 
     public UnionFind(int n) {
         id = new int[n];
         size = new int[n];
+        toCompress = new int[n];
         for (int i = 0; i < n; i++) {
             id[i] = i;
             size[i] = 1;
@@ -30,19 +32,32 @@ public class UnionFind {
     }
 
     public int find(int p) {
-        List<Integer> compressionList = new ArrayList<>();
+        int i = 0;
+        
         while (p != id[p]) {
-            compressionList.add(p);
+            toCompress[i++] = p;
             p = id[p];
         }
 
-        for (Integer e : compressionList)
-            id[e] = p;
+        for (int j = 0; j < i; j++)
+            id[j] = p;
 
         return p;
     }
 
     public boolean connected(int p, int q) {
         return find(p) == find(q);
+    }
+
+    public static void main(String[] args) {
+        UnionFind uf = new UnionFind(10);
+
+        assert !uf.connected(0, 1);
+
+        uf.union(0, 1);
+
+        assert uf.connected(0, 1);
+
+        assert uf.find(0) == 1;
     }
 }
